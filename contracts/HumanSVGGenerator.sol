0c95a06efdbbedbcbb64cb0b9cd81438ee7f4a10
chelorope <cmarcelorp@gmail.com>
2022-02-21 12:28:48 -0300

    SVG generator contract

---
+++ contracts/HumanSVGGenerator.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "base64-sol/base64.sol";
+
+contract HumanSVGGenerator {
+    string[] public hairs;
+    string[] public legs;
+    string[] public bodies;
+
+    constructor() {}
+
+    function generateHumanSVG(uint256 _randomness)
+        internal
+        returns (string memory humanSvg)
+    {
+        humanSvg = string(
+            abi.encodePacked(
+                "<svg width='380' height='480' xmlns='http://www.w3.org/2000/svg'>",
+                "<g transform='translate(43 35)' fill='none' fill-rule='evenodd'>",
+                "<g transform='translate(82 -7)'>",
+                // HEAD
+                "<path d='M62 65c-4-5-7-12-7-19 2-21 31-17 37-6s5 38-2 40c-3 1-10-1-16-5l4 29H54l8-39Z' fill='#B28B67'/>",
+                // HAIR
+                generateHairPath(_randomness),
+                "</g>",
+                // LEG
+                "<g transform='translate(0 187)'>",
+                generateBottomPath(_randomness),
+                "</g>",
+                // BODY
+                "<g transform='translate(22 82)'>",
+                generateBodyPath(_randomness),
+                "</g>",
+                "</g>",
+                "</svg>"
+            )
+        );
+    }
+}
